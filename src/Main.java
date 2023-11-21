ebcc75862d8103bf63c0d71fb24b895547c6958f
n3mochaken <ns.mixa@gmail.com>
2023-11-21 21:25:13 +0300

    ><

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
         Bicycle bicycle = new Bicycle("bicycle1",2);
         Bicycle bicycle2 = new Bicycle("bicycle2",2);
 
-        ServiceStation station = new ServiceStation();
-        station.check(car, null, null);
-        station.check(car2, null, null);
-        station.check(null, bicycle, null);
-        station.check(null, bicycle2, null);
-        station.check(null, null, truck);
-        station.check(null, null, truck2);
+        ServiceStation station = new ServicesCar();;
+        station.check(car);
+        station.check(car2);
+        station.check(bicycle);
+        station.check(bicycle2);
+        station.check(truck);
+        station.check(truck2);
     }
 }
--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,24 +1,10 @@
-
-public class ServiceStation {
-    public void check(Car car, Bicycle bicycle, Truck truck) {
-        if (car != null) {
-            System.out.println("Обслуживаем " + car.getModelName());
-            for (int i = 0; i < car.getWheelsCount(); i++) {
-                car.updateTyre();
-            }
-            car.checkEngine();
-        } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.getModelName());
-            for (int i = 0; i < truck.getWheelsCount(); i++) {
-                truck.updateTyre();
-            }
-            truck.checkEngine();
-            truck.checkTrailer();
-        } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.getModelName());
-            for (int i = 0; i < bicycle.getWheelsCount(); i++) {
-                bicycle.updateTyre();
-            }
-        }
+public interface ServiceStation {
+    default void check (Car car){
+        System.out.println("Легковушку не привезли");
+    }
+    default void check(Truck truck){
+        System.out.println("Грузовик не завезли");
+    }default void check(Bicycle bicycle){
+        System.out.println("Велосипед не завезли");
     }
 }
